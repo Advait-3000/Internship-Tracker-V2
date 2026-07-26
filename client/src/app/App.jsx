ccc4d054d6a4c7f940396096fe1e587bc1b5f581
sahil-111-p <sahilpachigar@gmail.com>
2026-07-27 00:08:08 +0530

    minor changes in folder structur

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -11,6 +11,7 @@ const App = () => {
       <ErrorBoundary>
         <BrowserRouter>
           <AppRouter />
+          <p>Hello DK</p>
         </BrowserRouter>
       </ErrorBoundary>
     </Provider>
